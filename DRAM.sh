dbf544df899da4617310ec1f5e108fe2626b3318
Tiffany du Plessis <84008482+enchantiff@users.noreply.github.com>
2021-10-07 10:07:12 +0200

    Update DRAM.sh

--- DRAM.sh
+++ DRAM.sh
@@ -1,1 +1,8 @@
+cd $PBS_O_WORKDIR
 
+module load DRAM
+source /apps/anaconda3-2020.02/etc/profile.d/conda.sh
+conda activate DRAM
+
+DRAM.py annotate -i '*.fa' -o annotation
+DRAM.py distill -i annotation/annotations.tsv -o genome_summaries --trna_path annotation/trnas.tsv --rrna_path annotation/rrnas.tsv
