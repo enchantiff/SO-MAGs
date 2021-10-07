1409bb7f96ca991ee464a8a77990061b2ee0fcf1
Tiffany du Plessis <84008482+enchantiff@users.noreply.github.com>
2021-10-07 10:01:30 +0200

    Update Atlas.sh

--- Atlas.sh
+++ Atlas.sh
@@ -2,6 +2,6 @@ module load metagenome-atlas
 source /apps/miniconda3/etc/profile.d/cond/sh
 conda activate atlasenv
 
-atlas init –db-dir databases /nlustre/users/tiffdp/SCALE-META-reads
+atlas init –db-dir databases /nlustre/users/tiffdp/SCALE-META-reads/atlas/ --working-dir /nlustre/users/tiffdp/SCALE-META-reads/atlas/SCALE-META-reads
 
-atlas run qc –-keep-going
+atlas run qc assembly binning --resources mem=250 --jobs 20 –-keep-going
